2d50f598a48b1289f123aec0f4fc32cd81c66106
Dmitry Naumov <nadima8@gmail.com>
2016-12-20 00:02:44 +0300

    add oxfor api integration

---
+++ Learn2Read/SoundTable/OxfordDictionary/OxfordDictionaryRequests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Learn2Read
+{
+    public class OxfordDictionaryRequests
+    {
+        public void GetTranscription(string word)
+        {
+            string url = $"https://od-api.oxforddictionaries.com:443/api/v1/entries/en/{word}/pronunciations";
+
+            using (var webClient = new WebClient())
+            {
+                webClient.QueryString.Add("Accept", "application/json");
+                webClient.QueryString.Add("app_id", "b9995a56");
+                webClient.QueryString.Add("app_key", "113e76a6337da3ba5897246665ecae67");
+                var response = webClient.DownloadString(url);
+                JObject json = JObject.Parse(response);
+                
+            }
+        }
+
+        
+    }
+}
---
+++ Learn2Read/SoundTable/OxfordDictionary/PronunciationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Learn2Read
+{
+    public class PronunciationModel
+    {
+        public string Trascription { get; set; }
+        public string AudioFile { get; set; }
+        public string Word { get; set; }
+        public string Category { get; set; }
+
+    }
+}
